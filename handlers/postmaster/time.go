ee7c473923e31d2a4840d966db03e016873f11e0
Jeremy Steele <jeremy.steele@istresearch.com>
2020-05-06 08:58:30 -0400

    Switch null time to 0

--- handlers/postmaster/time.go
+++ handlers/postmaster/time.go
@@ -17,7 +17,7 @@ func (t *ISO8601WithMilli) UnmarshalJSON(b []byte) error {
 
 	s := strings.Trim(string(b), "\"")
 
-	if s == "null" || len(s) == 0 {
+	if s == "0" || len(s) == 0 {
 		t.Time = time.Time{}
 		return nil
 	}
@@ -29,7 +29,7 @@ func (t *ISO8601WithMilli) UnmarshalJSON(b []byte) error {
 
 func (t *ISO8601WithMilli) MarshalJSON() ([]byte, error) {
 	if t.Time.UnixNano() == (time.Time{}).UnixNano() {
-		return []byte("null"), nil
+		return []byte("0"), nil
 	}
 
 	return []byte(fmt.Sprintf("\"%s\"", t.Time.Format(milliLayout))), nil
